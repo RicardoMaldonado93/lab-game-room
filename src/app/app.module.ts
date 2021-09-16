38b14033b9dd21145c2cdef929ea62a9857a8204
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2021-09-16 10:45:39 -0300

    implement lazy-loading in pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,24 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './modules/app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { ComponentsModule } from './modules/components.module';
-import { PagesModule } from './modules/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     ComponentsModule,
-    PagesModule,
-    BrowserAnimationsModule,
     CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
     HttpClientModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   bootstrap: [AppComponent],
-  providers:[]
+  providers: [],
 })
 export class AppModule {}
--- src/app/modules/components.module.ts
+++ src/app/modules/components.module.ts
@@ -1,14 +1,14 @@
-import { MaterialModule } from 'src/app/modules/angular-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/modules/angular-material.module';
 import { FooterComponent } from '../components/footer/footer.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { DialogComponent } from './../components/modal/modal.component';
 import { SpinnerComponent } from './../components/spinner/spinner.component';
-import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  imports: [CommonModule, BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [CommonModule, BrowserModule, MaterialModule, RouterModule],
   declarations: [NavbarComponent, FooterComponent, DialogComponent, SpinnerComponent] ,
   exports: [NavbarComponent, FooterComponent,SpinnerComponent ],
   providers:[SpinnerComponent],
